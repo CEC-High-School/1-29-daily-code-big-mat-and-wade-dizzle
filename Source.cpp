fb1a3a26a0225efb6b07b8623f5b62734bc7e8d0
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-01-29 13:00:07 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,38 @@
+#include<allegro5\allegro_font.h>
+#include<iostream>
+#include<allegro5\allegro_ttf.h>
+using namespace std;
+int age;
+int main()
+{
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	cout << "what is your age?" << endl;
+	cin >> age;
+
+	ALLEGRO_DISPLAY *AGE = NULL;
+	al_init();
+	al_init_font_addon();
+	al_init_ttf_addon();
+
+	ALLEGRO_FONT *font = al_load_ttf_font("StayWildy.ttf", 72, 0);
+	AGE = al_create_display(600, 480);
+	al_set_window_position(AGE, 200, 200);
+	
+
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+
+	
+		al_draw_textf(font, al_map_rgb(200, 40, 200), 300, 200, ALLEGRO_ALIGN_CENTRE, "Happy %d th Birthday", age);
+
+		al_flip_display();
+
+		al_rest(5);
+
+		al_destroy_display(AGE);
+
+
+
+
+
+}
